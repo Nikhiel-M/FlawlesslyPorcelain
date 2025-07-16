24cf17aee1dd665560c104e2772e8816ddc008c3
Nikhiel-M <164867669+Nikhiel-M@users.noreply.github.com>
2025-07-16 15:47:39 +0200

    edited styles and added new img

--- src/Components/Header/Header.styled.js
+++ src/Components/Header/Header.styled.js
@@ -45,3 +45,4 @@ export const HeaderWrapper = styled.header`
     text-decoration: underline;
   }
 `;
+
--- src/Home/Home.styled.js
+++ src/Home/Home.styled.js
@@ -1,19 +1,19 @@
-  import styled from "styled-components";
-  import lady from "../Assets/flawedpng.png"
+import styled from "styled-components";
+import lady from "../Assets/flawevV2.png";
 
- export const Container = styled.div`
- display: flex;
- justify-content: center;
- align-items: center;
- position: static;
- background-image: url(${lady});
- background-size: cover;
- background-repeat: no-repeat;
- flex-direction: column;
+export const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: static;
+  background-image: url(${lady});
+  background-size: contain;
+  background-position: top;
+  flex-direction: column;
+  margin-top: 13vh;
   height: 100vh;
   width: 100vw;
   padding: 0 2rem;
   text-align: center;
   margin-bottom: 5rem;
-  `
-
+`;
